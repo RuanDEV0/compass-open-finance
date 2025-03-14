4447e8e028557ecc653725240388b5b5837486a2
Ruan <oliveiraruan2018@gmail.com>
2025-03-14 13:01:37 -0400

    feat: install prompt-sync for input console

--- dominando-ferramentas/solutions-challenge/solutions-pt1.js
+++ dominando-ferramentas/solutions-challenge/solutions-pt1.js
@@ -2,3 +2,11 @@ let init = 0;
 let pending = 0;
 var test;
 const zero = "Testando";
+
+const ps = require("prompt-sync");
+const prompt = ps();
+
+
+let data = prompt("Enter name : ");
+
+console.log(`hello ${data}`);
