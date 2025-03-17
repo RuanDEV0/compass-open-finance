a973f5ba311092cfeddb718e08d6ec5745c5fedf
Ruan <oliveiraruan2018@gmail.com>
2025-03-17 10:35:50 -0400

    feat: function check if number is prime

--- dominando-ferramentas/solutions-challenge/solutions-pt1.js
+++ dominando-ferramentas/solutions-challenge/solutions-pt1.js
@@ -1,5 +1,20 @@
-const ps = require("prompt-sync");
-const prompt = ps();
+const user = require("prompt-sync");
+const prompt = user();
+
+// ## **1️⃣ Somar Todos os Elementos de um Array Unidimensional**
+
+let input = prompt("Enter elements array : ");
+// Remove colchetes da string de entrada do usuário
+let removeFirstColchete = input.replace("[", " ");
+let removeColchete = removeFirstColchete.replace("]", " ");
+
+let arrayFinal = removeColchete.split(",");
+
+if(!isNaN(sumArray(arrayFinal))){
+    console.log("Sum of elements: "+ sumArray(arrayFinal));
+}else{
+    console.log("Value invalid detected!");
+}
 
 function sumArray(array){
     let total = 0;
@@ -9,31 +24,21 @@ function sumArray(array){
     return total;
 }
 
-function inputData(){
 
-    let input = prompt("Enter elements array : ");
 
-    // Remove colchetes da string de entrada do usuário
-    let removeFirstColchete = input.replace("[", " ");
-    let removeColchete = removeFirstColchete.replace("]", " ");
-        
-    let data = removeColchete.split(",");
+//## **2️⃣ Verificar se um Número é Primo**
 
-    return data;
-}
+input = prompt("Enter number: ");
+let result = isNaN(input) ? "Value invalid detected!" : `${input} is number prime? ${isPrimo(input)}`
+console.log(result);
 
-function firstChallenge(){
-
-    let arrayFinal = inputData();
-
-    if(!isNaN(sumArray(arrayFinal))){
-        console.log("Sum of elements: "+ sumArray(arrayFinal));
-    }else{
-        console.log("Value invalid detected!");
+function isPrimo(number){
+    let cont = 0;
+    for(let i = 1; i <= number; i++){
+        if(number % i == 0){
+            cont++;
+        }
     }
-    
+
+    return cont == 2 ? true : false;
 }
-
-// ## **1️⃣ Somar Todos os Elementos de um Array Unidimensional**
-
-firstChallenge();
