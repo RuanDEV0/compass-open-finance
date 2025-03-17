1cf6a56d71bd6f32202c7ac2abb63cf47f3b7869
Ruan <oliveiraruan2018@gmail.com>
2025-03-17 09:43:14 -0400

    feat: function sum elements of array

--- dominando-ferramentas/solutions-challenge/solutions-pt1.js
+++ dominando-ferramentas/solutions-challenge/solutions-pt1.js
@@ -1,12 +1,39 @@
-let init = 0;
-let pending = 0;
-var test;
-const zero = "Testando";
-
 const ps = require("prompt-sync");
 const prompt = ps();
 
+function sumArray(array){
+    let total = 0;
+    for(let i = 0; i < array.length; i++){
+        total += parseInt(array[i]);
+    }
+    return total;
+}
 
-let data = prompt("Enter name : ");
+function inputData(){
 
-console.log(`hello ${data}`);
+    let input = prompt("Enter elements array : ");
+
+    // Remove colchetes da string de entrada do usuário
+    let removeFirstColchete = input.replace("[", " ");
+    let removeColchete = removeFirstColchete.replace("]", " ");
+        
+    let data = removeColchete.split(",");
+
+    return data;
+}
+
+function firstChallenge(){
+
+    let arrayFinal = inputData();
+
+    if(!isNaN(sumArray(arrayFinal))){
+        console.log("Sum of elements: "+ sumArray(arrayFinal));
+    }else{
+        console.log("Value invalid detected!");
+    }
+    
+}
+
+// ## **1️⃣ Somar Todos os Elementos de um Array Unidimensional**
+
+firstChallenge();
