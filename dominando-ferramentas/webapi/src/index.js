3b5b7375a3a9b60c31fd18982157b2829c5337fb
Ruan <oliveiraruan2018@gmail.com>
2025-03-24 11:51:35 -0400

    feat: solutions for route definition

--- dominando-ferramentas/webapi/src/index.js
+++ dominando-ferramentas/webapi/src/index.js
@@ -1,7 +1,33 @@
 const http = require('http');
 const PORT = 3000;
+const DEFAULT_HEADER = {'Content-Type': 'application-json'}
+
+const routes = {
+    '/heroes:GET': async (request, response) => {
+        const {id} = request.queryString;
+
+        console.log({id});
+        return response.end();
+    },
+    default: (request, response) => {
+        response.write('Route default');
+        response.end();
+    }
+}
+
 const handler = (request, response) => {
-    response.end('testing');
+    const {url, method } = request;
+    const [first, route, id] = url.split('/');
+
+    request.queryString = {id: isNaN(id) ? id : Number(id)};
+
+    const key = `/${route}:${method}`;
+
+    response.writeHead(200, DEFAULT_HEADER);
+
+    const chosen = routes[key] || routes.default;
+
+    return chosen(request, response);
 }
 
 http.createServer(handler).listen(PORT, () => console.log('server running at', PORT));
