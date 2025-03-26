ea454dea24d35f084d9bf7c2c5ad873451edf02b
Ruan <oliveiraruan2018@gmail.com>
2025-03-26 08:17:35 -0400

    feat: query params and path params

--- dominando-ferramentas/course-nodejs-express/module-01/src/index.js
+++ dominando-ferramentas/course-nodejs-express/module-01/src/index.js
@@ -3,8 +3,14 @@ const express = require('express');
 const server = express();
 const PORT = 3020;
 
-server.get('/product', (request, response) => {
-    response.end(JSON.stringify({message: 'Hellow Word'}));
+server.get('/product/:parameter', (request, response) => {
+
+    const queryParams = request.query.parameter;
+    const pathVariable = request.params.parameter;
+    response.end(JSON.stringify({
+        QueryParames: queryParams,
+        PathVariable: pathVariable
+        }));
 });
 
 server.listen(3020);
