e95aa6e6f842b5976b81a2605b42d665bcf49c93
Ruan <oliveiraruan2018@gmail.com>
2025-03-27 10:54:50 -0400

    feat: connecting with container mongodb

--- dominando-ferramentas/course-nodejs-express/api-devhouse/backend/src/app.js
+++ dominando-ferramentas/course-nodejs-express/api-devhouse/backend/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App{
     constructor(){
         this.server = express();
+            
+        mongoose.connect('mongodb://admin:admin@localhost:27017/')
+            .then(() => console.log('database connected'))
+            .catch(error => console.log(error));
 
         this.middlewares();
         this.routes();
