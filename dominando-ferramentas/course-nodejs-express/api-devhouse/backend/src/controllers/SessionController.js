3ddbc6049eb14c2317dd1f3fd82c5a5d77fb7f33
Ruan <oliveiraruan2018@gmail.com>
2025-03-28 08:07:55 -0400

    feat: methods of sessions user

---
+++ dominando-ferramentas/course-nodejs-express/api-devhouse/backend/src/controllers/SessionController.js
@@ -0,0 +1,29 @@
+/*
+    metodos: index, show, store, destroy and update
+
+    index: listagem de sessoes
+    show: listar uma sessao
+    store: criar uma sessao
+    update: atualizar uma sessao
+    destroy: deletar uma sessao
+ */
+
+import User from '../model/User'
+
+class SessionController{
+    async store(request, response){
+        const { email } = request.body;
+
+        let user = await User.findOne({ email });
+
+        if(!user){
+            user = await User.create({ email });
+        }
+
+        return response.json(user);
+    }
+}
+
+
+
+export default new SessionController();
