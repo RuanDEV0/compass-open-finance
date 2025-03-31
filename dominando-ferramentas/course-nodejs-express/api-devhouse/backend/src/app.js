ed127adebd739a4877e07d22ff8eda9b0e38c35b
Ruan <oliveiraruan2018@gmail.com>
2025-03-31 08:23:06 -0400

    build: add cors and configuration for api public

--- dominando-ferramentas/course-nodejs-express/api-devhouse/backend/src/app.js
+++ dominando-ferramentas/course-nodejs-express/api-devhouse/backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import mongoose from 'mongoose';
 import routes from './routes';
@@ -16,6 +17,7 @@ class App{
     }
 
     middlewares(){
+        this.server.use(cors());
         this.server.use(
             '/files', express.static(path.resolve(__dirname, '../thumbnails'))
         );
