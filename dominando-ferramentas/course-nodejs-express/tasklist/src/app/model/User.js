df3934aefa0a3d7e5132e516c602a6c1bcd5dc04
Ruan <oliveiraruan2018@gmail.com>
2025-04-01 11:00:53 -0400

    feat(bcryptjs): add feature hashing password

--- dominando-ferramentas/course-nodejs-express/tasklist/src/app/model/User.js
+++ dominando-ferramentas/course-nodejs-express/tasklist/src/app/model/User.js
@@ -1,14 +1,20 @@
 import Sequelize, { Model } from 'sequelize';
-
+import bcrypt from 'bcryptjs';
 class User extends Model{
     static init(sequelize){
         super.init({
             name: Sequelize.STRING,
             email: Sequelize.STRING,
+            password: Sequelize.VIRTUAL,
             password_hash: Sequelize.STRING
         },{
             sequelize
         });
+        this.addHook('beforeSave', async (User) => {
+            User.password_hash = await bcrypt.hash(User.password, 6);
+        })
+
+        return this;
     }
 }
 
